5316baa5ff3d9e7db6dbf6c46efba6ab09397147
Vish Potnis <vishpotnis@gmail.com>
2021-12-23 01:49:33 -0500

    day16 part 2

--- day-16/day16.py
+++ day-16/day16.py
@@ -1,9 +1,20 @@
 import sys
 from enum import Enum
+from functools import reduce
 
 class PacketType(Enum):
     LITERAL = 1
     OPERATOR = 2
+
+class PacketIDType(Enum):
+    SUM = 0
+    PRODUCT = 1
+    MINIMUM = 2
+    MAXIMUM = 3
+    LITERAL = 4
+    GREATER = 5
+    LESS = 6
+    EQUAL = 7
 
 class PacketParserType(Enum):
     NUM_PACKETS = 1
@@ -14,14 +25,27 @@ class Packet():
     def __init__(self, pkt_type : PacketType, parsed_pkt):
         self.pkt_type = pkt_type
         self.subpkts = []
+        self.value = None
         if self.pkt_type == PacketType.LITERAL:
             self.set_literal_pkt(parsed_pkt)
         elif self.pkt_type == PacketType.OPERATOR:
             self.set_operator_pkt(parsed_pkt)
 
+        self.evaluate_pkt_map = {
+            PacketIDType.SUM        :   self.evaluate_sum_pkt,
+            PacketIDType.PRODUCT    :   self.evaluate_product_pkt,   
+            PacketIDType.MINIMUM    :   self.evaluate_minimum_pkt,
+            PacketIDType.MAXIMUM    :   self.evaluate_maximum_pkt,
+            PacketIDType.LITERAL    :   self.evaluate_literal_pkt,
+            PacketIDType.GREATER    :   self.evaluate_greater_pkt,
+            PacketIDType.LESS       :   self.evaluate_less_pkt,
+            PacketIDType.EQUAL      :   self.evaluate_equal_pkt
+        }
+
     def set_literal_pkt(self, parsed_pkt):
         self.version = parsed_pkt[0]
         self.id = parsed_pkt[1]
+        self.id_type = PacketIDType(self.id)
         self.literal_arr = parsed_pkt[2]
         self.literal_val = 0
         for val in self.literal_arr:
@@ -30,6 +54,7 @@ class Packet():
     def set_operator_pkt(self, parsed_pkt):
         self.version = parsed_pkt[0]
         self.id = parsed_pkt[1]
+        self.id_type = PacketIDType(self.id)
         self.length_id = parsed_pkt[2]
         if self.length_id == 0:
             self.total_length = parsed_pkt[3]
@@ -43,9 +68,9 @@ class Packet():
             pkt_txt = f"LITERAL PKT: version {self.version}, id {self.id}, literal {self.literal_val}"
         elif self.pkt_type == PacketType.OPERATOR:
             if self.length_id == 0:
-                pkt_txt = f"OPERATOR PKT: version {self.version}, id {self.id}, length id {self.length_id}, total length {self.total_length} bits"
+                pkt_txt = f"OPERATOR PKT: version {self.version}, id {self.id} ({self.id_type.name}), length id {self.length_id}, total length {self.total_length} bits"
             elif self.length_id == 1:
-                pkt_txt =  f"OPERATOR PKT: version {self.version}, id {self.id}, length id {self.length_id}, num packets {self.num_subpkts}"
+                pkt_txt =  f"OPERATOR PKT: version {self.version}, id {self.id} ({self.id_type.name}), length id {self.length_id}, num packets {self.num_subpkts}"
         
         text += header + pkt_txt + '\n'
         for subpkt in self.subpkts:
@@ -54,8 +79,40 @@ class Packet():
 
     def __str__(self):
         return self.get_string_rep(0)
-        
 
+
+    def evaluate_sum_pkt(self):        
+        self.value = reduce(lambda a, b: a + b, map(lambda pkt: pkt.get_value(), self.subpkts))
+
+    def evaluate_product_pkt(self):        
+        self.value = reduce(lambda a, b: a * b, map(lambda pkt: pkt.get_value(), self.subpkts))
+
+    def evaluate_minimum_pkt(self):        
+        self.value = reduce(lambda a, b: min(a, b), map(lambda pkt: pkt.get_value(), self.subpkts))
+
+    def evaluate_maximum_pkt(self):
+        self.value = reduce(lambda a, b: max(a, b), map(lambda pkt: pkt.get_value(), self.subpkts))
+
+    def evaluate_literal_pkt(self):
+        self.value = self.literal_val
+
+    def evaluate_greater_pkt(self):
+        self.value = int(self.subpkts[0].get_value() > self.subpkts[1].get_value())
+            
+    def evaluate_less_pkt(self):
+        self.value = int(self.subpkts[0].get_value() < self.subpkts[1].get_value())            
+
+    def evaluate_equal_pkt(self):        
+        self.value = int(self.subpkts[0].get_value() == self.subpkts[1].get_value())
+
+    def evaluate_pkt(self):
+        self.evaluate_pkt_map[self.id_type]()
+
+    def get_value(self):
+        if self.value is None:
+            self.evaluate_pkt()
+        return self.value
+        
 
 class PacketParser():
 
@@ -84,6 +141,7 @@ class PacketParser():
         id = self.parse_num_bits(3, advance_idx)
         if not advance_idx:
             self.idx -= 3
+            self.num_bits_read -= 3
         return version, id
 
     def parse_literal_packet(self):
@@ -142,7 +200,7 @@ class PacketParser():
 
 
     def parse_type_take_until(self):
-        while int(self.text[self.idx:], 2) != 0:
+        while len(self.text[self.idx:]) and int(self.text[self.idx:], 2) != 0:
             self.parse_pkt()
         return self.pkts, self.idx, self.num_bits_read
 
@@ -177,14 +235,16 @@ def main():
     fname = sys.argv[1]
     bin_str = parse_input(fname)
 
-    # istrs = ['D2FE28', '38006F45291200', 'EE00D40C823060', '8A004A801A8002F478', '620080001611562C8802118E34', 'C0015000016115A2E0802F182340', 'A0016C880162017C3686B18A3D4780']
-    # bin_str = parse_input_from_str(istrs[6])
+    # istrs = ['D2FE28', '38006F45291200', 'EE00D40C823060', '8A004A801A8002F478', '620080001611562C8802118E34', 'C0015000016115A2E0802F182340', 'A0016C880162017C3686B18A3D4780',
+            #  'C200B40A82', '04005AC33890', '880086C3E88112', 'CE00C43D881120', 'D8005AC2A8F0', 'F600BC2D8F', '9C005AC2F8F0', '9C0141080250320F1802104A08']    
+    # bin_str = parse_input_from_str(istrs[7])
 
     parser = PacketParser(bin_str)
     parsed_pkts, _, _ = parser.parse()
 
     for pkt in parsed_pkts:
         print(pkt)
+        print(f"Packet value: {pkt.get_value()}")
         
     print(f"Version sum: {get_version_sum(PacketParser.all_pkts)}")
 
